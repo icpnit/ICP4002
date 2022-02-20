86279758b3283ef52de3663c7ed80db8a1d71f4d
Alireza Jafartash <alirezaopmc@gmail.com>
2022-02-20 21:15:07 +0330

    scrips are removed
    they are organized in a separate folder

--- gen.sh
+++
@@ -1,9 +0,0 @@
-# generates testcases for specified problem path
-# usaage: sh gen.sh path/to/folder
-cd $1
-
-g++ generator.cpp
-
-./a.out
-
-rm ./a.out
--- new.sh
+++
@@ -1,3 +0,0 @@
-# make a problem direcroty and copy the generator template into it
-mkdir -p ./src/$1
-cp ./tests/problems/aplusb/generator.cpp ./src/$1
