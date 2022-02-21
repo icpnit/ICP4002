b3a6f2c35ff827b7cb84c864c0168509298ba0d0
Alireza Jafartash <alirezaopmc@gmail.com>
2022-02-22 02:57:19 +0330

    more and better logging for tst scrip

--- scripts/tst.sh
+++ scripts/tst.sh
@@ -3,11 +3,26 @@
 N=$(ls $1/in | wc -l)
 
 i=1
+wrong=0
 while [ "$i" -lt "$N" ]; do
   cat "$1/in/input$i.txt" | python3 "$1/solution.py" > tmp.txt
-  rst=$(cmp tmp.txt "./$1/out/output$i.txt")
-  if [ ! -z "$rst" ]; then echo "Failed on test $i"; fi
+  out=$(cat tmp.txt)
+
+  if [ -z "$out" ]; then
+    wrong=1;
+    echo "> $i-th testcase failed"
+  fi
+  
+  if [ ! -z "$out" ]; then
+    rst=$(cmp tmp.txt "./$1/out/output$i.txt");
+  fi
+  
+  if [ ! -z "$out" ] && [ ! -z "$rst" ]; then
+    echo "> $i-th testcase failed"; wrong=1;
+  fi
   i=$((i+1))
 done
+
+if [ $wrong -lt 1 ]; then echo "Tests passed"; fi
 
 rm -rf tmp.txt
