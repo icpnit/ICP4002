bbd1c7d2d4626ed0143b2422119e751ebaaa5679
Alireza Jafartash <alirezaopmc@gmail.com>
2022-03-06 02:29:30 +0330

    generate script updated

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -5,6 +5,7 @@ rm -rf $1/out
 
 cpp=$(ls $1 | grep cpp$)
 if [ ! -z "$cpp" ]; then
+  echo "> C++ generator found"
   g++ $1/generator.cpp -o generator
   ./generator
   rm ./generator
@@ -13,6 +14,9 @@ fi
 
 py=$(ls $1 | grep py$)
 if [ ! -z "$py" ]; then
-  python3 $1/generator.py
+  echo "> Python generator found"
+  python3 $1/generator.py $1
   exit
 fi
+
+echo "! No generator found"
